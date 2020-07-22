662fda127713e65eb758a92e0f2085bbcf7e4d4c
Mario23junior <mariojunior3251@gmail.com>
2020-07-22 03:11:29 -0300

    Editar / Eliminar/ produtos

--- src/main/java/br/com/crudCont/Controller/ProductController.java
+++ src/main/java/br/com/crudCont/Controller/ProductController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.crudCont.Model.Product;
 import br.com.crudCont.Service.ProductService;
@@ -37,7 +39,45 @@ public class ProductController {
 		service.save(product);
 		return "redirect:/";
 	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") long id) {
+	      ModelAndView mav = new ModelAndView("edit_product");
+	      Product product = service.get(id);
+	      mav.addObject("product", product);
+	      return mav;
+    }
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") long id) {
+		service.delete(id);
+		return "redirect:/";
+	}
 }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
