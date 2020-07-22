eb52ec5cee2aff6203bfc724b5e2f6a4ee4fac99
Mario23junior <mariojunior3251@gmail.com>
2020-07-22 02:06:35 -0300

    Salvando produtos no banco de dados

--- src/main/java/br/com/crudCont/Controller/ProductController.java
+++ src/main/java/br/com/crudCont/Controller/ProductController.java
@@ -5,21 +5,42 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.crudCont.Model.Product;
 import br.com.crudCont.Service.ProductService;
 
 @Controller
 public class ProductController {
-    
+
 	@Autowired
 	private ProductService service;
-	
+
 	@RequestMapping("/")
 	public String viewHomePage(Model model) {
 		List<Product> listProducts = (List<Product>) service.listAll();
-		model.addAttribute("listProducts",listProducts);
+		model.addAttribute("listProducts", listProducts);
 		return "index";
 	}
+
+	@RequestMapping("/new")
+	public String showNewProductPage(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		return "new_product";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		service.save(product);
+		return "redirect:/";
+	}
 }
+
+
+
+
+
+
